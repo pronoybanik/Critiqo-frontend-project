e9661b2c7d025acb8886a06e8346c766b69e8aa5
PRONOY <pronoybanik82@gmail.com>
2025-05-06 21:35:22 +0600

    solve error

--- src/services/Review/index.ts
+++ src/services/Review/index.ts
@@ -6,18 +6,6 @@ import { revalidateTag } from "next/cache";
 import { cookies } from "next/headers";
 
 
-export const createReview = async (data: any) => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/reviews`, {
-    method: "POST",
-    headers: {
-      Authorization: (await cookies()).get("accessToken")?.value || "",
-    },
-    body: data,
-  });
-  revalidateTag("REVIEW");
-  return res.json();
-};
-
 
 //get all review
 export const getAllReview = async (page?: string, limit?: string, query?: { [key: string]: string | string[] | undefined }) => {
